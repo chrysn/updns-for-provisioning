4722f45eae695a536600ce8e6d9f820743dea025
chrysn <chrysn@fsfe.org>
2021-04-22 06:52:30 +0000

    Serve any ACME request with data from /tmp/token
    This is obviously not production ready, but good enough for practical
    demos.

--- src/main.rs
+++ src/main.rs
@@ -231,6 +231,17 @@ async fn proxy(buf: &[u8]) -> Result<Vec<u8>> {
 }
 
 async fn get_answer(domain: &str, query: QueryType) -> Option<DnsRecord> {
+    if domain.starts_with("_acme-challenge.") && query == QueryType::TXT {
+        // Something about this is malformed according to wireshark, but everyone eats it. could be
+        // related to the OPT record queried along.
+        let key = dbg!(std::fs::read("/tmp/token")).ok()?;
+        return Some(dbg!(DnsRecord::TXT {
+            domain: domain.to_string(),
+            txt: key,
+            ttl: 20,
+        }));
+    }
+
     if let Some(ip) = HOSTS.read().await.get(domain) {
         match query {
             QueryType::A => {
